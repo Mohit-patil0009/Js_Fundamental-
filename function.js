b233a6ce5eb997b4a2d9cd1384171556a03199ba
Mohit-patil0009 <patilmohit0009@gmail.com>
2025-12-09 12:36:07 +0530

    add function file

---
+++ function.js
@@ -0,0 +1,11 @@
+// Normal function declaration
+function add(a, b) {
+    return a + b;
+}   
+add(10,10); // 20
+
+// fat arrow function
+const multiply = (a, b) => {
+    return a * b;
+}
+multiply(10,10); // 100

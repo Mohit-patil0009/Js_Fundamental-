4226915dfcf3185ef5709109e734672c6ca6e59d
Mohit-patil0009 <patilmohit0009@gmail.com>
2025-12-12 08:59:45 +0530

    add file

---
+++ stringMethod.js
@@ -0,0 +1,89 @@
+/*
+let str = "Hello  world"
+
+let length= str.length
+console.log("🚀 ~ length:", length)
+*/
+
+/*
+let str = "Hello world"
+
+// let subString = str.slice(2,5)
+// let subString = str.substring(2,5)
+console.log("🚀 ~ subString:", subString)
+
+*/
+
+/*
+let str = "Hello world"
+
+let newStr = str.concat("!!!","---")
+console.log("🚀 ~ newStr:", newStr)
+*/
+
+
+/*
+let str = "Hello world";
+
+// let newStr = str.toUpperCase()
+let newStr = str.toLowerCase();
+console.log("🚀 ~ newStr:", newStr);
+
+let name = "Urvish";
+
+let searchName = "urvish";
+console.log("------>", searchName.toLowerCase() === name.toLowerCase());
+*/
+
+/*
+let str = "   Hello world    ";
+let newStr = str.trim();
+console.log("🚀 ~ newStr:", newStr)
+*/
+
+/*
+let str = "Hello world";
+// let index = str.indexOf("o",5);
+// let index = str.lastIndexOf("o");
+console.log("🚀 ~ index:", index)
+*/
+
+/*
+
+let str = "Hello world, @ welcome / to the * world of JavaScript";
+
+// let str2 = str.replace("world", "Universe");
+// let str2 = str.replaceAll("world", "Universe");
+// let str2 = str.replaceAll(/[@/*]/g, "----");
+let str2 = str.replace(/[@/*]/g, "----");
+
+console.log("🚀 ~ str:", str2)
+*/
+
+
+/*
+let str = "Hello world, welcome to the world of JavaScript";
+
+let availabe = str.includes("World");
+console.log("🚀 ~ availabe:", availabe)
+*/
+
+/*
+let str = "Hello world, welcome to the world of JavaScript";
+
+let ans = str.startsWith(" Hello");
+console.log("🚀 ~ ans:", ans)
+*/
+
+let str = "Hello world,  welcome to the world of JavaScript";
+
+// let arr = str.split("world");
+// let arr = str.split(" ");
+// let arr = str.split("");
+console.log("🚀 ~ arr:", arr)
+
+
+// hello world => dlorw olleh
+// hello world => world hello
+// hello    world ji   !! => hello world ji !!
+

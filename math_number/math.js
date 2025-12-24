d4d7518163ff06b67bc8f26672385d0e33a8fee9
Mohit-patil0009 <patilmohit0009@gmail.com>
2025-12-24 13:51:41 +0530

    three file added

---
+++ math_number/math.js
@@ -0,0 +1,32 @@
+// let maxValu = Math.max(1,2,3,4,5,0.4)
+// console.log("🚀 ~ maxValu:", maxValu)
+
+
+// let minValu = Math.min(1,2,3,4,5,0.4,-1)
+// console.log("🚀 ~ minValu:", minValu)
+
+// let x = Math.floor(2.9) 
+// let x = Math.floor(2.4) 
+// console.log("🚀 ~ x:", x)
+
+// let x = Math.ceil(2.9) 
+// let x = Math.ceil(2.4)  
+// let x = Math.ceil(2.01)  
+// let x = Math.ceil(2.0)  
+// console.log("🚀 ~ x:", x)
+
+
+// let x = Math.round(2.9)  
+// let x = Math.round(2.5)  
+// let x = Math.round(2.4)  
+// console.log("🚀 ~ x:", x)
+
+// let x = Math.random()  // 0 - 1
+ 
+// console.log("🚀 ~ x:", x)
+
+
+// otp generator => 6 => 6 digit
+// if user pass 6 then create 6 digit otp
+// if user pass 4 then create 4 digit otp
+
---
+++ math_number/number.js
@@ -0,0 +1,42 @@
+// let ans = Number.isInteger(-1.4)
+// console.log("🚀 ~ ans:", ans)
+
+// let ans = Number.parseFloat("1.5")
+// console.log("🚀 ~ ans:", ans)
+
+// let ans = Number.parseInt("1.5") // 1
+// console.log("🚀 ~ ans:", ans)
+
+// let x = "1.5"
+// console.log("🚀 ~ x:", +x)
+
+
+// let x = 10.263488263278362879384
+
+// let y = x.toFixed(5)
+// console.log("🚀 ~ y:",x, y)
+
+
+/*
+
+variable and type let var const
+data type
+condition
+loop
+function
+operator
+string method
+array method
+number method
+math method
+promis
+promish handling
+error handling
+then catch finally
+call back function
+async/await
+syncronus/asyncronus
+HOC
+currying
+
+*/
